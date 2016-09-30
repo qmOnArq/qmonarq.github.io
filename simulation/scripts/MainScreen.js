a2c08a71d2f6ab19870d06d7dd3ad4266111b6f3
SadMeow <@>
2016-10-01 00:26:16 +0200

    1

--- simulation/scripts/MainScreen.js
+++ simulation/scripts/MainScreen.js
@@ -2,7 +2,11 @@ class MainScreen extends Screen {
 
     constructor(engine) {
         super(engine);
-        this.objects.push(new Sprite(this, Assets['buildings'], 'bazar'));
-        this.objects.push(new Sprite(this, Assets['buildings'], 'bazar', 100, 200, false))
+        this.objects.push(new Sprite(this, Assets['buildings'], 'bazar', 100, 200));
+        this.objects.push(new Sprite(this, Assets['buildings'], 'bazar', 100, 400, false))
+        this.objects.push(new Sprite(this, Assets['buildings'], 'bazar', 400, 200))
+        this.objects.push(new Sprite(this, Assets['buildings'], 'bazar', 400, 400, false))
+        this.objects[2].scale = 2
+        this.objects[3].scale = 2
     }
 }
--- simulation/scripts/Sprite.js
+++ simulation/scripts/Sprite.js
@@ -12,9 +12,15 @@ class Sprite extends Object {
         this.animate = (typeof animate !== 'undefined') ? animate : true;
         this.x = (typeof x !== 'undefined') ? x : 100;
         this.y = (typeof y !== 'undefined') ? y : 100;
+        this.width = this.data.width;
+        this.height = this.data.height;
+        this.paused = false;
+        this.scale = 1;
+        this.alpha = 1;
     }
 
     update(delta) {
+        if (this.paused) return;
         if (this.animate) {
             this.timePassed += delta;
             while (this.timePassed >= 0.032) {
@@ -38,17 +44,30 @@ class Sprite extends Object {
 
     render() {
         var ctx = this.screen.engine.ctx;
-        //TODO fix numbers, remove white around
-        ctx.drawImage(this.asset.img, 0, 0, 128, 128, this.x, this.y, 128, 128);
+        
+        var originalAlpha = ctx.globalAlpha;
+        ctx.globalAlpha *= this.alpha;
+
+        var startX = (this.asset.tileWidth / 2) - (this.width / 2);
+        var startY = (this.asset.tileHeight / 2) - (this.height / 2);
+        var x = this.x - this.width / 2 * this.scale;
+        var y = this.y - this.height * this.scale;
+        ctx.drawImage(this.asset.img,
+            startX, startY, this.width, this.height,
+            x, y, this.width * this.scale, this.height * this.scale);
 
         if (this.animate) {
             var tile = this.step + this.data.mainTiles;
             var tileX = Math.floor(tile % this.asset.tilesPerRow);
             var tileY = Math.floor(tile / this.asset.tilesPerRow) + this.data.startRow;
+            x = x + this.data.animOffsetX * this.scale;
+            y = y + this.data.animOffsetY * this.scale;
             ctx.drawImage(this.asset.img,
-                tileX * 128, tileY * 128, 128, 128,
-                this.x + this.data.animOffsetX, this.y + this.data.animOffsetY, 128, 128);
+                tileX * this.asset.tileWidth + startX, tileY * this.asset.tileHeight + startY, this.width, this.height,
+                x, y, this.width * this.scale, this.height * this.scale);
         }
+
+        ctx.globalAlpha = originalAlpha;
     }
 
 }
